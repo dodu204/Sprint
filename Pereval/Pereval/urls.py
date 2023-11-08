0532e1fec5d15a5a9ea3d3e9c929c9d2c2e01042
dodu204 <dodu204@mail.ru>
2023-11-08 18:53:05 +0300

    Изменения в urls файле. Убрано дублирование url по email.

--- Pereval/Pereval/urls.py
+++ Pereval/Pereval/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from rest_framework import routers
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from peak.views import PerevalViewSet, EmailAPIView
-from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'pereval', PerevalViewSet)
-router.register(r'submitData/email', EmailAPIView, basename='email-pereval')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
